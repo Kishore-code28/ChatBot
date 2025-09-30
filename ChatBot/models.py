3f9d243a1933fce98a3687acee90853173fcb5b3
Kishore-code28 <kishoreparameswaran2004@gmail.com>
2025-09-30 11:30:02 +0530

    Creating Superuser and some modifying

--- ChatBot/models.py
+++ ChatBot/models.py
@@ -2,9 +2,12 @@ from django.db import models
 
 
 class Products(models.Model):
-    product_id = models.IntegerField(primary_key= True,)
     customer_name = models.CharField(max_length=50)
     product_name = models.CharField(max_length=50)
     category = models.CharField(max_length=50)
     status = models.CharField(max_length=50)
     price = models.IntegerField()
+
+    def __str__(self):
+        return self.customer_name
+    
